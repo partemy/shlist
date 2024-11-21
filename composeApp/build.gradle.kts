18813647c3febf43bfce9b176166a27e0f879f9c
partemy <partemydev@gmail.com>
2024-11-22 00:29:07 +0300

    init

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
@@ -30,10 +31,18 @@ kotlin {
     sourceSets {
         
         androidMain.dependencies {
-
+            implementation(compose.preview)
+            implementation(libs.androidx.activity.compose)
         }
         commonMain.dependencies {
-
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+            implementation(libs.androidx.lifecycle.viewmodel)
+            implementation(libs.androidx.lifecycle.runtime.compose)
         }
     }
 }
